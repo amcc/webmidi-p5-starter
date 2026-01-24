d350a2335ad14e5dae87f7e56a5e9822a56d8e21
Alistair McClymont <alistairmcclymont@gmail.com>
2026-01-24 11:06:50 +0000

    commented midi

--- sketch.js
+++ sketch.js
@@ -1,32 +1,42 @@
 // we will use this to store the MIDI device once we find it
 let midiInput = null;
+
+// set some initial values for our sketch
 let circleSize = 100;
-let currentCircleSize = circleSize;
 let circleHue = 0;
 let circleStroke = 10;
-let currentCircleStroke = circleStroke;
 let bgColour = 0;
-let damp = 0.1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
 
+  // ebable WebMidi.js
   WebMidi.enable()
     .then(onEnabled)
     .catch((err) => alert(err));
 }
 
+// SEE THE COMMENTED OUT CODE IN onEnabled() FOR HELP FINDING YOUR DEVICE NAME
+// this function is called once WebMidi.js is ready to use
 function onEnabled() {
   if (WebMidi.inputs.length < 1) {
     console.log("no midi devices found");
     return;
   }
 
+  // WHEN STARTING OUT USE THE CODE BELOW TO FIND YOUR DEVICE NAME
+  // THE LOOP BELOW WILL LOG EVERY CONNECTED MIDI DEVICE TO THE CONSOLE
+  // WebMidi.inputs.forEach((device, index) => {
+  //   console.log(index + ": " + device.name);
+  // });
+
+  // ADD YOUR DEVICE NAME INSTEAD OF "MPD218 Port A"
   midiInput = WebMidi.getInputByName("MPD218 Port A");
 
+  // if we dont find the device, exit
   if (!midiInput) {
-    console.log("MPD218 not found");
+    console.log("Device not found");
     return;
   }
 
@@ -39,23 +49,20 @@ function draw() {
   background(bgColour, 100, 100);
 
   stroke(0);
-  strokeWeight(currentCircleStroke);
+  strokeWeight(circleStroke);
   fill(circleHue, 100, 100);
 
-  currentCircleStroke = lerp(currentCircleStroke, circleStroke, damp);
-  currentCircleSize = lerp(currentCircleSize, circleSize, damp);
-
-  circle(width / 2, height / 2, currentCircleSize);
+  circle(width / 2, height / 2, circleSize);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+// MIDI event handlers
+// these are called whenever a MIDI message is received
+// the 'note', 'pot', and 'touch' objects contain useful information about the message
 function noteOn(note) {
-  // note.value = velocity (0–1)
-  // note.note.number = which pad
-
   if (note.note.number === 48) {
     circleSize = midiMap(note.value, 0, width);
     circleHue = midiMap(note.value, 0, 360);
@@ -74,8 +81,9 @@ function controlChange(pot) {
   }
 }
 
-function pressure(pad) {
-  circleStroke = midiMap(pad.value, 0, 100);
+function pressure(touch) {
+  console.log(touch);
+  circleStroke = midiMap(touch.value, 0, 100);
 }
 
 // MIDI values are always 0–1
