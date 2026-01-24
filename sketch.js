97478d88576c67963eafad1c6d37f03793a24178
Alistair McClymont <alistairmcclymont@gmail.com>
2026-01-24 11:14:13 +0000

    simple version needs work

--- sketch.js
+++ sketch.js
@@ -1,93 +1,65 @@
-// we will use this to store the MIDI device once we find it
+// MIDI device
 let midiInput = null;
+let isConnected = false;
 
-// set some initial values for our sketch
+// Circle properties
 let circleSize = 100;
-let circleHue = 0;
-let circleStroke = 10;
-let bgColour = 0;
+let circleColor = 100;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
+  createCanvas(800, 600);
 
-  // ebable WebMidi.js
+  // Connect to MIDI
   WebMidi.enable()
     .then(onEnabled)
-    .catch((err) => alert(err));
+    .catch((err) => console.log(err));
 }
 
-// SEE THE COMMENTED OUT CODE IN onEnabled() FOR HELP FINDING YOUR DEVICE NAME
-// this function is called once WebMidi.js is ready to use
 function onEnabled() {
   if (WebMidi.inputs.length < 1) {
-    console.log("no midi devices found");
+    console.log("No MIDI devices found");
     return;
   }
 
-  // WHEN STARTING OUT USE THE CODE BELOW TO FIND YOUR DEVICE NAME
-  // THE LOOP BELOW WILL LOG EVERY CONNECTED MIDI DEVICE TO THE CONSOLE
-  // WebMidi.inputs.forEach((device, index) => {
-  //   console.log(index + ": " + device.name);
-  // });
+  // Connect to first available MIDI device
+  midiInput = WebMidi.inputs[2];
+  isConnected = true;
+  console.log("Connected to: " + midiInput.name);
 
-  // ADD YOUR DEVICE NAME INSTEAD OF "MPD218 Port A"
-  midiInput = WebMidi.getInputByName("MPD218 Port A");
-
-  // if we dont find the device, exit
-  if (!midiInput) {
-    console.log("Device not found");
-    return;
-  }
-
-  midiInput.addListener("noteon", noteOn);
-  midiInput.addListener("controlchange", controlChange);
-  midiInput.addListener("channelaftertouch", pressure);
+  // Listen for knob turns (controller change)
+  midiInput.addListener("controlchange", onKnobTurn);
 }
 
 function draw() {
-  background(bgColour, 100, 100);
+  // Background
+  background(220);
 
-  stroke(0);
-  strokeWeight(circleStroke);
-  fill(circleHue, 100, 100);
-
+  // Draw circle
+  fill(circleColor, 150, 200);
+  noStroke();
   circle(width / 2, height / 2, circleSize);
+
+  // Show connection status
+  fill(0);
+  textSize(14);
+  text(isConnected ? "✓ MIDI Connected" : "⚠ No MIDI Device", 20, 30);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// MIDI event: knob turn
+// Knob 3 (CC 3) = size
+// Knob 9 (CC 9) = color
+function onKnobTurn(event) {
+  console.log(event);
+  // Convert MIDI value (0-127) to 0-1 range
+  let midiValue = event.value;
 
-// MIDI event handlers
-// these are called whenever a MIDI message is received
-// the 'note', 'pot', and 'touch' objects contain useful information about the message
-function noteOn(note) {
-  if (note.note.number === 48) {
-    circleSize = midiMap(note.value, 0, width);
-    circleHue = midiMap(note.value, 0, 360);
+  // Knob 3 controls circle size
+  if (event.controller.number === 3) {
+    circleSize = map(midiValue, 0, 1, 50, 400);
   }
-}
 
-function controlChange(pot) {
-  if (pot.controller.number === 3) {
-    circleSize = midiMap(pot.value, 0, width);
-  } else if (pot.controller.number === 9) {
-    circleHue = midiMap(pot.value, 0, 360);
-  } else if (pot.controller.number === 12) {
-    circleStroke = midiMap(pot.value, 0, 100);
-  } else if (pot.controller.number === 13) {
-    bgColour = midiMap(pot.value, 360, 0);
+  // Knob 9 controls circle color
+  if (event.controller.number === 9) {
+    circleColor = map(midiValue, 0, 1, 0, 255);
   }
-}
-
-function pressure(touch) {
-  console.log(touch);
-  circleStroke = midiMap(touch.value, 0, 100);
-}
-
-// MIDI values are always 0–1
-// This converts them into useful screen values
-function midiMap(value, min, max) {
-  return map(value, 0, 1, min, max);
 }
