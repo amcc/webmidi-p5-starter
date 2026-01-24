959361e293222ae00713dd1f2c3aaee947cfa24b
Alistair McClymont <alistairmcclymont@gmail.com>
2026-01-24 10:59:45 +0000

    version with lerp

--- sketch.js
+++ sketch.js
@@ -12,35 +12,39 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
 
-  // enable WebMidi.js
   WebMidi.enable()
     .then(onEnabled)
     .catch((err) => alert(err));
+}
 
-  // Function triggered when WEBMIDI.js is ready
-  function onEnabled() {
-    // Display available MIDI input devices
-    if (WebMidi.inputs.length < 1) {
-      console.log("no midi devices found");
-    } else {
-      // find all the inputs and log them to the console
-      // WebMidi.inputs.forEach((device, index) => {
-      //   console.log(index + ": " + device.name);
-      // });
-
-      midiInput = WebMidi.getInputByName("MPD218 Port A");
-      midiInput.addListener("noteon", (e) => noteOn(e));
-      midiInput.addListener("controlchange", (e) => controlChange(e));
-    }
+function onEnabled() {
+  if (WebMidi.inputs.length < 1) {
+    console.log("no midi devices found");
+    return;
   }
+
+  midiInput = WebMidi.getInputByName("MPD218 Port A");
+
+  if (!midiInput) {
+    console.log("MPD218 not found");
+    return;
+  }
+
+  midiInput.addListener("noteon", noteOn);
+  midiInput.addListener("controlchange", controlChange);
+  midiInput.addListener("channelaftertouch", pressure);
 }
 
 function draw() {
   background(bgColour, 100, 100);
-  fill(circleHue, 100, 100);
-  currentCircleStroke = lerp(currentCircleStroke, circleStroke, damp);
+
+  stroke(0);
   strokeWeight(currentCircleStroke);
+  fill(circleHue, 100, 100);
+
+  currentCircleStroke = lerp(currentCircleStroke, circleStroke, damp);
   currentCircleSize = lerp(currentCircleSize, circleSize, damp);
+
   circle(width / 2, height / 2, currentCircleSize);
 }
 
@@ -49,24 +53,33 @@ function windowResized() {
 }
 
 function noteOn(note) {
-  console.log(note.value, note.velocity);
+  // note.value = velocity (0–1)
+  // note.note.number = which pad
+
   if (note.note.number === 48) {
-    circleSize = map(note.value, 0, 1, 0, width);
-    circleHue = map(note.value, 0, 1, 0, 360);
+    circleSize = midiMap(note.value, 0, width);
+    circleHue = midiMap(note.value, 0, 360);
   }
 }
 
 function controlChange(pot) {
   if (pot.controller.number === 3) {
-    circleSize = map(pot.value, 0, 1, 0, width);
+    circleSize = midiMap(pot.value, 0, width);
+  } else if (pot.controller.number === 9) {
+    circleHue = midiMap(pot.value, 0, 360);
+  } else if (pot.controller.number === 12) {
+    circleStroke = midiMap(pot.value, 0, 100);
+  } else if (pot.controller.number === 13) {
+    bgColour = midiMap(pot.value, 360, 0);
   }
-  if (pot.controller.number === 9) {
-    circleHue = map(pot.value, 0, 1, 0, 360);
-  }
-  if (pot.controller.number === 12) {
-    circleStroke = map(pot.value, 0, 1, 0, 100);
-  }
-  if (pot.controller.number === 13) {
-    bgColour = map(pot.value, 0, 1, 360, 0);
-  }
+}
+
+function pressure(pad) {
+  circleStroke = midiMap(pad.value, 0, 100);
+}
+
+// MIDI values are always 0–1
+// This converts them into useful screen values
+function midiMap(value, min, max) {
+  return map(value, 0, 1, min, max);
 }
